736599d7a476848fe94631eefbf1017ec4153f9e
rahul <nizarerahul7@gmail.com>
2024-10-29 21:54:36 +0530

    new changes

--- app.js
+++ app.js
@@ -40,3 +40,27 @@ function prevSlide(){
 prev.addEventListener("click",prevSlide);
 
 const autoSlider = setInterval(nextSlide , 3000);
+
+
+// ========swiper js=======================
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
