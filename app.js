35ab2025bd77f991030af48a23807326d63793bf
rahul <nizarerahul7@gmail.com>
2024-10-30 23:35:48 +0530

    new changes

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+const tooltipTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="tooltip"]'
+);
+const tooltipList = [...tooltipTriggerList].map(
+  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+);
 
 // ================slider part==================
 let prev = document.getElementById("prev");
@@ -11,74 +15,82 @@ let contents = document.querySelectorAll(".content .item");
 let rotate = 0;
 let active = 0;
 let countItem = items.length;
-let rotateAdd = 360/countItem;
+let rotateAdd = 360 / countItem;
 
-function show(){
-    image.style.setProperty('--rotate', rotate + 'deg');
-    contents.forEach((content, key)=>{
-        // console.log(content)
-        if(key==active){
-            content.classList.add("active")
-        }
-        else{
-            content.classList.remove("active")
-        }
-    })  
+function show() {
+  image.style.setProperty("--rotate", rotate + "deg");
+  contents.forEach((content, key) => {
+    // console.log(content)
+    if (key == active) {
+      content.classList.add("active");
+    } else {
+      content.classList.remove("active");
+    }
+  });
 }
-function nextSlide(){
-    active = active + 1 > countItem -1 ? 0 : active + 1;
-    rotate = rotate + rotateAdd; 
-    show()
+function nextSlide() {
+  active = active + 1 > countItem - 1 ? 0 : active + 1;
+  rotate = rotate + rotateAdd;
+  show();
 }
 next.addEventListener("click", nextSlide);
 
-function prevSlide(){
-    active = active -1 < 0 ? countItem -1 : active -1;
-    rotate = rotate - rotateAdd;
-    show()
+function prevSlide() {
+  active = active - 1 < 0 ? countItem - 1 : active - 1;
+  rotate = rotate - rotateAdd;
+  show();
 }
-prev.addEventListener("click",prevSlide);
+prev.addEventListener("click", prevSlide);
 
-const autoSlider = setInterval(nextSlide , 3000);
-
+const autoSlider = setInterval(nextSlide, 3000);
 
 // ========swiper js=======================
-const swiper = new Swiper('.swiper', {
-    direction: 'horizontal',
-    loop: true,
-    slidesPerView: 4,
-      spaceBetween: 10,
-      freeMode: {
-        enabled: true,
-        sticky: true,
-    },
-    autoplay: {
-        delay: 3000,
-        disableOnInteraction: false,
-      },
-      speed : 600,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-  
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    
+const swiper = new Swiper(".swiper", {
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 4,
+  spaceBetween: 10,
+  allowTouchMove : true,
+  freeMode: {
+    enabled: true,
+    sticky: true,
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: true,
+  },
+  speed: 800,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// ========swiper play on click==============
+const cards = document.querySelectorAll(".swiper-slide");
+
+cards.forEach((card) => {
+  card.addEventListener("click", () => {
+    // console.log("is stop");
+    swiper.autoplay.stop();
   });
-
-  // ========swiper play on click==============
-  const cards = document.querySelectorAll(".swiper-slide")
-
-  cards.forEach(card =>{
-    card.addEventListener("click", ()=>{
-      // console.log("is stop");
-      swiper.autoplay.stop();
-    })
-    card.addEventListener("mouseleave", ()=>{
-      swiper.autoplay.start();
+  card.addEventListener("mouseleave", () => {
+    swiper.autoplay.start();
+  });
+});
+// =================counter cart add =============
+  const quantity = document.querySelectorAll(".quantity");
+  const decrease = document.querySelectorAll(".decrease");
+  const increase = document.querySelectorAll(".increase");
+  let count = 1;
+  increase.forEach((incBtn , index )=>{
+    incBtn.addEventListener("click", ()=>{
+      count++;
+      quantity[index].textContent = count;
     })
   })
