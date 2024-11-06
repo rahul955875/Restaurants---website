2f30496a0c384420e0789c5f9af0ca0aafa45040
rahul <nizarerahul7@gmail.com>
2024-11-06 18:56:02 +0530

    change in about us

--- app.js
+++ app.js
@@ -25,44 +25,47 @@ function hoverEff(){
 }
 hoverEff()
 // ================ slider part ==================
-let prev = document.getElementById("prev");
-let next = document.getElementById("next");
-let image = document.querySelector(".images");
-let items = document.querySelectorAll(".images .item");
-let contents = document.querySelectorAll(".content .item");
-
-let rotate = 0;
-let active = 0;
-let countItem = items.length;
-let rotateAdd = 360 / countItem;
-
-function show() {
-  image.style.setProperty("--rotate", rotate + "deg");
-  contents.forEach((content, key) => {
-    console.log(content)
-    if (key == active) {
-      content.classList.add("active");
-    } else {
-      content.classList.remove("active");
-    }
-  });
+function sliderRound(){
+  let prev = document.getElementById("prev");
+  let next = document.getElementById("next");
+  let image = document.querySelector(".images");
+  let items = document.querySelectorAll(".images .item");
+  let contents = document.querySelectorAll(".content .item");
+  
+  let rotate = 0;
+  let active = 0;
+  let countItem = items.length;
+  let rotateAdd = 360 / countItem;
+  
+  function show() {
+    image.style.setProperty("--rotate", rotate + "deg");
+    contents.forEach((content, key) => {
+      console.log(content)
+      if (key == active) {
+        content.classList.add("active");
+      } else {
+        content.classList.remove("active");
+      }
+    });
+  }
+  function nextSlide() {
+    active = active + 1 > countItem - 1 ? 0 : active + 1;
+    rotate = rotate + rotateAdd;
+    show();
+  }
+  next.addEventListener("click", nextSlide);
+  
+  function prevSlide() {
+    active = active - 1 < 0 ? countItem - 1 : active - 1;
+    rotate = rotate - rotateAdd;
+    show();
+  }
+  prev.addEventListener("click", prevSlide);
+  
+  const autoSlider = setInterval(nextSlide, 4000);
+  
 }
-function nextSlide() {
-  active = active + 1 > countItem - 1 ? 0 : active + 1;
-  rotate = rotate + rotateAdd;
-  show();
-}
-next.addEventListener("click", nextSlide);
-
-function prevSlide() {
-  active = active - 1 < 0 ? countItem - 1 : active - 1;
-  rotate = rotate - rotateAdd;
-  show();
-}
-prev.addEventListener("click", prevSlide);
-
-const autoSlider = setInterval(nextSlide, 4000);
-
+sliderRound()
 // ======== swiper js for menu =======================
 const swiper = new Swiper(".swiper", {
   direction: "horizontal",
